778d9a3150b67959705b199078f0cf6a900fed60
Sam Taylor <sjltaylor@gmail.com>
2023-07-04 09:29:41 +0100

    toggle filters and print filter set

--- coldmod-web/src/controls_ui.rs
+++ coldmod-web/src/controls_ui.rs
@@ -1,32 +1,58 @@
+use std::collections::HashMap;
+
 use leptos::*;
 
 #[component]
 pub fn ControlsUI(cx: Scope) -> impl IntoView {
-    // let dispatch = use_context::<Dispatch>(cx).unwrap();
+    let keys = vec![vec!["COLD"], vec!["P10", "P20", "P40", "P90", "HOT"]];
+    let mut initial_filter_state = HashMap::<&str, bool>::new();
+    for group in keys.iter() {
+        for key in group {
+            initial_filter_state.insert(key, false);
+        }
+    }
+
+    let rw_filters = create_rw_signal(cx, initial_filter_state);
+
+    create_effect(cx, move |_| {
+        log!("filter changed: {:?}", rw_filters.get());
+    });
 
     return view! {cx,
     <div class="area controls">
         <div class="container controls">
             <div class="buttons">
-                <div class="button-group">
-                    <ToggleButton label="COLD" />
-                </div>
-                <div class="button-group pxx-hot">
-                    <ToggleButton label="P10" />
-                    <ToggleButton label="P20" />
-                    <ToggleButton label="P40" />
-                    <ToggleButton label="P90" />
-                    <ToggleButton label="HOT" />
-                </div>
+                {keys.into_iter().map(|group| {
+                    view! {cx,
+                    <div class="button-group">
+                        {group.into_iter().map(|key| {
+                            let (is_on,w_is_on) = create_slice(cx, rw_filters,
+                                |h| {
+                                    *h.get(key).unwrap()
+                                },
+                                |h, n| {
+                                    h.insert(key, n);
+                                })
+                            ;
+
+                            view! {cx,
+                                <ToggleButton label=key.clone() is_on w_is_on />
+                            }
+                        }).collect_view(cx)}
+                    </div>
+                }}).collect_view(cx)}
             </div>
         </div>
     </div> };
 }
 
 #[component]
-pub fn ToggleButton(cx: Scope, #[prop(into)] label: String) -> impl IntoView {
-    let (is_on, w_is_on) = create_signal(cx, false);
-
+pub fn ToggleButton(
+    cx: Scope,
+    #[prop(into)] label: String,
+    is_on: Signal<bool>,
+    w_is_on: SignalSetter<bool>,
+) -> impl IntoView {
     let class_name = move || {
         if is_on.get() {
             return "toggle-button on";
@@ -36,7 +62,7 @@ pub fn ToggleButton(cx: Scope, #[prop(into)] label: String) -> impl IntoView {
 
     return view! {cx,
 
-    <div class={class_name} on:click=move |_| w_is_on.update(|v| *v = !*v)>
+    <div class={class_name} on:click=move |_| w_is_on.set(!is_on.get())>
         <div class="toggle-button-label">{label}</div>
     </div> };
 }
